2bff8f6496c44191f988bc9fe3aeec2a9b61a0da
Gilly <gillianohea@gmail.com>
2018-08-21 20:26:52 +0000

    update all files

--- home_and_away/static/js/graph.js
+++ home_and_away/static/js/graph.js
@@ -9,12 +9,8 @@ function makeGraphs(error, testhaadb) {
     }
 
 //clean date data
-//     var yearFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
+
     testhaadb.forEach(function (d) {
-        // d.date = yearFormat(d.date);
-        // d.Year = d.date.getFullYear();
-        // // d["year_died"] = yearFormat.parse(d["year_died"]);
-        // // // d["year_died"].setDate(1);
         d["year_died"] = +d["year_died"];
     });
 
@@ -31,9 +27,7 @@ function makeGraphs(error, testhaadb) {
     var fNameDim = ndx.dimension(function (d) {
         return d["first"];
     });
-    // var lNameDim = ndx.dimension(function (d) {
-    //     return d["last"];
-    // });
+   
     var genderDim = ndx.dimension(function (d) {
         return d["gender"];
     });
@@ -49,9 +43,7 @@ function makeGraphs(error, testhaadb) {
         return d["storyline"];
     });
 
-    // var yearDim = ndx.dimension(function (d) {
-    //     return d["year_died"];
-    // });
+   
     var yearDim = ndx.dimension(function (d) {
         return d["year_died"] ? d["year_died"] : 0;
     });
@@ -72,13 +64,7 @@ function makeGraphs(error, testhaadb) {
         return d["marital_stat"];
     });
 
-    // var causeofinjDim = ndx.dimension(function (d) {
-    //     return d["cause_of_inj"];
-    // });
-    //
-    // var typeofinjDim = ndx.dimension(function (d) {
-    //     return d["type_of_inj"];
-    // });
+   
 
     var deadinjDim = ndx.dimension(function (d) {
         return d["deadinj"];
@@ -88,14 +74,7 @@ function makeGraphs(error, testhaadb) {
 
     var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
 
-    // testhaadb.forEach(function (d) {
-    //     d["year_died"] = dateFormat.parse(d["year_died"]);
-    //     d["year_died"].setDate(1);
-    //     d["deadinj"] = +d["deadinj"];
-    // });
-
-
-
+   
     //Calculate metrics
     var numDeathsByGender = genderDim.group();
     var numDeathsByCause = causeOfDeathDim.group();
@@ -105,25 +84,10 @@ function makeGraphs(error, testhaadb) {
     var numDeathsByYear = yearDim.group();
     var numDeathsByStat = maritalDim.group();
     var numDeathsByFname = fNameDim.group();
-    // var numDeathsByLname = lNameDim.group();
     var numDeathsBySline = storyDim.group();
     var numDeathsByPartner = partnerDim.group();
-    // var numInjsByCause = causeofinjDim.group();
-    // var numInjsByType = typeofinjDim.group();
     var numDeadorInj = deadinjDim.group();
-    // var deathsByYear = yearDim.group().reduceSum(function (d) {
-    //     return d["total_deaths"];
-    // });
-    // var deathGroup = yearDim.group();
-    //
-    // var all = ndx.groupAll();
-    // var totalDeaths = ndx.groupAll().reduceSum(function (d) {
-    //     return d["total_deaths"];
-    // });
-    // var all = ndx.groupAll()
-    // var totalDeaths = ndx.groupAll().reduceSum(function (d){
-    //     return d["deadinj"];
-    // });
+    
 
 
 
@@ -134,27 +98,18 @@ function makeGraphs(error, testhaadb) {
     var deathChart = dc.pieChart("#death-chart");
     var houseChart = dc.pieChart("#house-chart");
     var yearChart = dc.barChart("#year-chart")
-    //var yearChart = dc.pieChart("#year-chart");
     var maritalChart = dc.pieChart("#marital-chart");
     var minDate = yearDim.bottom(1)[0]["year_died"];
     var maxDate = yearDim.top(1)[0]["year_died"];
     var fnameChart = dc.rowChart("#fname-chart");
-    // var lnameChart = dc.rowChart("#lname-chart");
     var storyChart = dc.pieChart("#story-chart");
     var partnerChart = dc.pieChart("#partner-chart");
-    // var causeinjChart = dc.pieChart("#causeinj-chart");
-    // var typeinjChart = dc.pieChart("#typeinj-chart");
     var deadorinjChart = dc.pieChart("#deadinj-chart");
-    // var totalDeathsND = dc.numberDisplay("#total-deaths-nd");
-    //
-    //
-    // totalDeathsND
-    //     .formatNumber(d3.format("d"))
-    //     .valueAccessor(function (d) {
-    //         return d;
-    //     })
-    //     .group(all);
+   
 
+    
+    
+    
     jobChart
         .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
         .height(220)
@@ -162,7 +117,8 @@ function makeGraphs(error, testhaadb) {
         .innerRadius(40)
         .transitionDuration(1500)
         .dimension(jobDim)
-        .group(numDeathsByJob);
+        .group(numDeathsByJob)
+        .legend(dc.legend());
 
     maritalChart
         .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
@@ -171,7 +127,8 @@ function makeGraphs(error, testhaadb) {
         .innerRadius(40)
         .transitionDuration(1500)
         .dimension(maritalDim)
-        .group(numDeathsByStat);
+        .group(numDeathsByStat)
+        .legend(dc.legend());
 
     houseChart
         .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
@@ -180,7 +137,8 @@ function makeGraphs(error, testhaadb) {
         .innerRadius(60)
         .transitionDuration(1500)
         .dimension(houseDim)
-        .group(numDeathsByHouse);
+        .group(numDeathsByHouse)
+        .legend(dc.legend());
 
     genderChart
         .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
@@ -189,44 +147,31 @@ function makeGraphs(error, testhaadb) {
         .innerRadius(40)
         .transitionDuration(1500)
         .dimension(genderDim)
-        .group(numDeathsByGender);
+        .group(numDeathsByGender)
+        .legend(dc.legend());
 
-    // causeinjChart
-    //     .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-    //     .height(220)
-    //     .radius(90)
-    //     .innerRadius(40)
-    //     .transitionDuration(1500)
-    //     .dimension(causeofinjDim)
-    //     .group(numInjsByCause);
-    //
-    // typeinjChart
-    //     .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-    //     .height(220)
-    //     .radius(90)
-    //     .innerRadius(40)
-    //     .transitionDuration(1500)
-    //     .dimension(typeofinjDim)
-    //     .group(numInjsByType);
+   
 
     deadorinjChart
-        .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
+        .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef", "#777"])
         .height(400)
         .radius(120)
         .innerRadius(10)
         .transitionDuration(1500)
         .dimension(deadinjDim)
-        .group(numDeadorInj);
+        .group(numDeadorInj)
+        .legend(dc.legend());
 
     deathChart
-        .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef", "#777"])
+        .ordinalColors(["#ff5359", "#ff6e4a", "#ff7538", "#ffa343", "#ffcf48", "#fdfc74", "#b2ec5d", "#1df914", "#1cac78", "#1cd3a2", "#1fcecb", "#1dacd6", "#1f75fe", "#5d76cb", "#7442c8", "#8f509d", "#fb7efd", "#ff1dce", "#c0448f", "#ff43a4", "#f75394", "#e3256b", "#de5d83", "#c8385a", "#fc2847", "#ff9baa", "#cb4154"])
         .height(800)
         .width(800)
         .radius(250)
         .innerRadius(0)
         .transitionDuration(1500)
         .dimension(causeOfDeathDim)
-        .group(numDeathsByCause);
+        .group(numDeathsByCause)
+        .legend(dc.legend());
 
     familyChart
         .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
@@ -240,24 +185,16 @@ function makeGraphs(error, testhaadb) {
 
 
     storyChart
-        .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-        .height(600)
-        .width(600)
-        .radius(200)
+        .ordinalColors(["#ff5359", "#ff6e4a", "#ff7538", "#ffa343", "#ffcf48", "#fdfc74", "#b2ec5d", "#1df914", "#1cac78", "#1cd3a2", "#1fcecb", "#1dacd6", "#1f75fe", "#5d76cb", "#7442c8", "#8f509d", "#fb7efd", "#ff1dce", "#c0448f", "#ff43a4", "#f75394", "#e3256b", "#de5d83", "#c8385a", "#fc2847", "#ff9baa", "#cb4154"])
+
+        .height(800)
+        .width(800)
+        .radius(250)
         // .innerRadius(60)
         .transitionDuration(1500)
         .dimension(storyDim)
-        .group(numDeathsBySline);
-
-
-    // yearChart
-    //     .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-    //     .height(600)
-    //     .radius(100)
-    //     .innerRadius(40)
-    //     .transitionDuration(1500)
-    //     .dimension(yearDim)
-    //     .group(numDeathsByYear);
+        .group(numDeathsBySline)
+        .legend(dc.legend());
 
 
     partnerChart
@@ -267,7 +204,8 @@ function makeGraphs(error, testhaadb) {
         .innerRadius(40)
         .transitionDuration(1500)
         .dimension(partnerDim)
-        .group(numDeathsByPartner);
+        .group(numDeathsByPartner)
+        .legend(dc.legend());
 
 
     fnameChart
@@ -278,26 +216,6 @@ function makeGraphs(error, testhaadb) {
         .dimension(fNameDim)
         .group(numDeathsByFname)
         .xAxis().ticks(3);
-
-    // lnameChart
-    //     .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-    //     .height(400)
-    //     .width(400)
-    //     .transitionDuration(1500)
-    //     .dimension(lNameDim)
-    //     .group(numDeathsByLname)
-    //     .xAxis().ticks(3);
-
-
-    // yearChart
-    //     .ordinalColors(["#0060ca", "#fcdc74", "#91ceff", "#ff4d4d", "#74dbef"])
-    //     .height(500)
-    //     .width(800)
-    //     .transitionDuration(1500)
-    //     .dimension(yearDim)
-    //     .group(numDeathsByYear)
-    //     .xAxis().ticks(6);
-
 
     yearChart
         .ordinalColors(["#374283", "#fff000"])
@@ -313,15 +231,16 @@ function makeGraphs(error, testhaadb) {
         .xUnits(function () {
             return 30;
         })
-        // .transitionDuration(500)
-        // makes the bar chart clickable, solution found on StackOverflow and mentioned in README
+        .transitionDuration(500)
+        //makes the bar chart clickable, solution found on StackOverflow and mentioned in README
         .on("renderlet", function (yearChart) {
         yearChart.selectAll("rect.bar").on("click", yearChart.onClick);
         })
         .centerBar(true)
         .elasticY(true)
         .renderHorizontalGridLines(true)
-        .xAxisLabel("Year")
+        .xAxisLabel("Year of death or injury")
+        .yAxisLabel("Strewth, deaths or injurys per year")
         .yAxis()
         .ticks(8);
         // .x(d3.time.scale().domain([minDate, maxDate]))
